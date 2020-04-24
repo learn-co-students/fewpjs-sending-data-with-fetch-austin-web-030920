10f752ffc61d35450a6f8d2845aa185403acf2bb
github username <github email address>
2020-04-24 10:33:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+
+configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      dogName: "Byron",
+      dogBreed: "Poodle"
+    })
+  };
+
+console.log(configurationObject)
+
+
+
+
+
+function submitData(userName, userEmail) {
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        
+        body: JSON.stringify({ name: userName, email: userEmail })
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json.id)
+        document.getElementsByTagName("body")[0].textContent = json.id
+        
+    })
+    .catch(function(error) {
+        document.getElementsByTagName("body")[0].textContent = error.message
+    });
+
+    
+}
